194d79ba15f9f46ad65eb612f63acfb3745b841d
Leo_455 <Leo_455@163.com>
2025-06-02 13:54:02 +0800

    add:加入了简单的校验 fix:行数为7或8时少删了一个校验词

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 本项目仅供[明日方舟](https://ak.hypergryph.com/)PV4解谜使用，目前可以正确解码Mode0和解谜中的两个Mode5条码，不一定能够正常解码其他的Code49条码。
 
-本项目基于Future攻坚组的[code49-decoder](https://github.com/theTeamFuture/code49-decoder)开发。加入了对Mode5的支持以正确解码一阶段条码，并对非数据字符判断进行了修改以解码二阶段条码。
+本项目基于Future攻坚组的[code49-decoder](https://github.com/theTeamFuture/code49-decoder)开发。加入了对Mode5的支持以正确解码二阶段条码，并对非数据字符判断进行了修改以解码三阶段条码。
 
 ## 如何使用
 
--- main.py
+++ main.py
@@ -11,34 +11,54 @@ def decode(code: list) -> str:
         解码完的结果
     """
 
+    global success
+    success = True
+
     code_value = []  # 存储条码的值
     for i, row in enumerate(code):
         # Extract pattern, remove head '11' and tail '4'
-        ptrn = row[2:-1]
+        if len(row) != 35:
+            print(f"[ERROR]: Row{i + 1} length incorrect")
+            success = False
+
+        ptrn = row
+        if row[:2] == '11':
+            ptrn = ptrn[2:]
+        if row[-1:] == '4':
+            ptrn = ptrn[:-1]
 
         # For each symbol
         row_value = []  # 存储每行的值
         for j in range(4):
             # Get value
             value = None
-            if c49.ROW_PARITY[i][j] == '0' or len(code) == i + 1:
-                value = c49.EVEN_PARITY_PATTERNS.index(ptrn[j << 3:(j + 1) << 3])
-            else:
-                value = c49.ODD_PARITY_PATTERNS.index(ptrn[j << 3:(j + 1) << 3])
+            try:
+                if c49.ROW_PARITY[i][j] == '0' or len(code) == i + 1:
+                    value = c49.EVEN_PARITY_PATTERNS.index(ptrn[j << 3:(j + 1) << 3])
+                else:
+                    value = c49.ODD_PARITY_PATTERNS.index(ptrn[j << 3:(j + 1) << 3])
+            except ValueError as err:
+                print(f"[ERROR]: Row{i + 1} Word{j + 1} {err} Please Check pattern!")
+                value = 'N/A'
+                success = False
 
             row_value.append(value)
         code_value.append(row_value)
 
     code_character_value = []  # 存储条码字符值
-    print("Value:")
+    print("\nValue:")
     for i, row_value in enumerate(code_value):
         print(f"Row{i + 1}: {row_value}")
 
         row_character_value = []  # 存储每行的字符值
         for value in row_value:
-            c1, c2 = value // 49, value % 49
-            row_character_value.append(c1)
-            row_character_value.append(c2)
+            if value != 'N/A':
+                c1, c2 = value // 49, value % 49
+                row_character_value.append(c1)
+                row_character_value.append(c2)
+            else:
+                row_character_value.append('N/A')
+                row_character_value.append('N/A')
 
         code_character_value.append(row_character_value)
 
@@ -48,12 +68,14 @@ def decode(code: list) -> str:
     for row_character_value in code_character_value:
         row_sum = 0
         for character_value in row_character_value[:-1]:
-            row_sum += character_value
+            if character_value != 'N/A':
+                row_sum += character_value
 
         if row_sum % 49 == row_character_value[-1]:
             print(f"Row{i + 1}: {row_character_value} Correct!")
         else:
             print(f"Row{i + 1}: {row_character_value} [ERROR]")
+            success = False
         i += 1
 
     # 将字符值解码为字符
@@ -63,24 +85,40 @@ def decode(code: list) -> str:
 
         # 计算最后一行的模式数字
         if i + 1 == row_len:
-            starting_mode = row_character_value[-2] - 7 * (row_len - 2)
+            if row_character_value[-2] != 'N/A':
+                starting_mode = row_character_value[-2] - 7 * (row_len - 2)
+                if starting_mode < 0 or starting_mode > 6:
+                    starting_mode = 'N/A'
+            else:
+                starting_mode = 'N/A'
 
         # 将每行的前7个字符值解码为字符
         # 忽略每行的最后一个字符（校验字符）
         for j, character_value in enumerate(row_character_value):
             if j != 7:
-                code_character.append(c49.CHAR_MAP[character_value])
+                if character_value != 'N/A':
+                    code_character.append(c49.CHAR_MAP[character_value])
+                else:
+                    code_character.append('N/A')
 
-    # 模式5需要在最开头加入一个S2
+    # 处理起始模式
     print(f"\nMode:{starting_mode}")
     match starting_mode:
         case 0:
             pass
+        case 4:
+            code_character.insert(0, 'S1')
         case 5:
             code_character.insert(0, 'S2')
+        case 'N/A':
+            print("decode as mode 0")
+            success = False
 
     # 删除校验词与模式字符
-    code_character[-5:] = []
+    if row_len <= 6:  # 行数小于等于6时有两个校验词
+        code_character[-5:] = []
+    elif row_len == 7 or row_len == 8:  # 行数为7或8时有三个校验词
+        code_character[-7:] = []
     print("\nCode Character(Without Check Characters):")
     print(code_character)
 
@@ -89,19 +127,23 @@ def decode(code: list) -> str:
     buffer = None
     output = ''
     for char in code_character:
+        if char != 'N/A':
+            if buffer == 'S2' or buffer == 'S1':  # 前一个字符为S1/S2 相加后解码
+                output += chr(c49.ASCII_CHAR_MAP.index(buffer + char))
 
-        if buffer == 'S2' or buffer == 'S1':  # 前一个字符为S1/S2 相加后解码
-            output += chr(c49.ASCII_CHAR_MAP.index(buffer + char))
+            else:  # 前一个字符不是S1/S2 判断当前字符
+                if char == 'S2' or char == 'S1':  # 当前字符是S1/S2 直接进行下一个循环
+                    pass
+                elif char == '<NS>':
+                    break
+                else:
+                    output += chr(c49.ASCII_CHAR_MAP.index(char))
 
-        else:  # 前一个字符不是S1/S2 判断当前字符
-            if char == 'S2' or char == 'S1':  # 当前字符是S1/S2 直接进行下一个循环
-                pass
-            elif char == '<NS>':
-                break
-            else:
-                output += chr(c49.ASCII_CHAR_MAP.index(char))
-
-        buffer = char
+            buffer = char
+        else:
+            buffer = ''
+            output += '[N/A]'
+            success = False
 
     return output
 
@@ -131,5 +173,9 @@ if __name__ == '__main__':
     # ]
 
     text = decode(code)
-    print("\nResult:")
+    if success is True:
+        print("\ndecode SUCCESS!")
+    elif success is False:
+        print("\nERROR occurred during decode The result might be wrong.")
+    print("Result:")
     print(text)
